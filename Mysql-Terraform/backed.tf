b4af5b349687bed5df4e1bbf421e4e04b79d720f
aayushverma191 <aayushverma4481@gmail.com>
2024-12-13 00:49:34 +0530

    Update backed.tf

--- Mysql-Terraform/backed.tf
+++ Mysql-Terraform/backed.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "kwibnwuygwby"
+    bucket = "batch28-final-tool"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
